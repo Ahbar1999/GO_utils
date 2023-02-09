ff7ec8dbd83c359b78c44a625b19c1f88ce59448
Ahbar1999 <39911199+Ahbar1999@users.noreply.github.com>
2023-02-09 12:36:07 +0000

    "added max function"

--- go_utils.go
+++ go_utils.go
@@ -10,7 +10,7 @@ func Min[A constraints.Ordered](arr *[]A, size int) (error, any) {
 
 	if size < 1 {
 		// var returnSize A = int(size)
-		return errors.New("Invalid size array provided"), size
+		return errors.New("invalid size array provided"), size
 	}
 
 	minEle := (*arr)[0]
@@ -18,6 +18,25 @@ func Min[A constraints.Ordered](arr *[]A, size int) (error, any) {
 	for i := 0; i < size; i += 1 {
 		val := (*arr)[i]
 		if minEle > val {
+			minEle = val
+		}
+	}
+
+	return nil, minEle
+}
+
+func Max[A constraints.Ordered](arr *[]A, size int) (error, any) {
+
+	if size < 1 {
+		// var returnSize A = int(size)
+		return errors.New("invalid size array provided"), size
+	}
+
+	minEle := (*arr)[0]
+
+	for i := 0; i < size; i += 1 {
+		val := (*arr)[i]
+		if minEle < val {
 			minEle = val
 		}
 	}
--- go_utils_test.go
+++ go_utils_test.go
@@ -27,3 +27,29 @@ func TestMinOnEmptyArray(t *testing.T) {
 		t.Fatalf(`Call to Min() failed for chars: %v`, chars)
 	}
 }
+
+func TestMaxOnIntegers(t *testing.T) {
+	nums := []int{12, 4, 1, 51, 0, -11}
+	want := 51
+	err, ans := Max(&nums, len(nums))
+	if err != nil && ans != want {
+		t.Fatalf(`Call to Max() failed for nums: %v`, nums)
+	}
+}
+
+func TestMaxOnBytes(t *testing.T) {
+	chars := []byte{'b', 'd', 'e', 'A', 'B', 'a'}
+	want := byte('a')
+	err, ans := Max(&chars, len(chars))
+	if err != nil && ans != want {
+		t.Fatalf(`Call to Max() failed for nums: %v`, chars)
+	}
+}
+
+func TestMaxOnEmptyArray(t *testing.T) {
+	chars := []int{}
+	err, size := Max(&chars, len(chars))
+	if err != nil && size != len(chars) {
+		t.Fatalf(`Call to Max() failed for chars: %v`, chars)
+	}
+}
